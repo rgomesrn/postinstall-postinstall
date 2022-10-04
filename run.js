9642c6df1f3ee4452d98d29dff05b69e1a673095
Rodrigo Gomes <105390949+rgomesrn@users.noreply.github.com>
2022-10-04 12:37:21 -0300

    Update run.js

--- run.js
+++ run.js
@@ -1,29 +1,32 @@
-const fs = require('fs')
-const cwd = require('process').cwd()
-const path = require('path')
-const exec = require('child_process').execSync
+const fs = require("fs");
+const cwd = require("process").cwd();
+const path = require("path");
+const exec = require("child_process").execSync;
 
-const appPath = path.normalize(cwd.slice(0, cwd.lastIndexOf('node_modules')))
+const appPath = path.normalize(cwd.slice(0, cwd.lastIndexOf("node_modules")));
 
-const packageJsonPath = path.join(appPath, 'package.json')
+const packageJsonPath = path.join(appPath, "package.json");
 
 if (fs.existsSync(packageJsonPath)) {
-  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath))
+  const packageJson = JSON.parse(fs.readFileSync(packageJsonPath));
 
   if (packageJson.scripts && packageJson.scripts.postinstall) {
-    const pkgManager = shouldUseYarn() ? 'yarn' : 'npm';
-    exec(`${pkgManager} run postinstall`, {cwd: appPath, encoding: 'utf8', stdio: 'inherit'}, (err, stdout, stderr) => {
-      console.log("errr is: ", err)
-      console.log("\nstdout is: ", stdout)
-      console.log("\nstderr is: ", stderr)
-      console.log("\n\n\n")
-    })
+    const pkgManager = shouldUseYarn() ? "yarn" : "npm";
+    try {
+      exec(`${pkgManager} run postinstall`, { cwd: appPath });
+    } catch (error) {
+      console.log("errr status is: ", error.status);
+      console.log("\nerrr message is: ", error.message);
+      console.log("\nstdout is: ", error.stdout.toString());
+      console.log("\nstderr is: ", error.stderr.toString());
+      console.log("\n\n\n");
+    }
   }
 }
 
 function shouldUseYarn() {
   try {
-    exec('yarnpkg --version', { stdio: 'ignore' });
+    exec("yarnpkg --version", { stdio: "ignore" });
     return true;
   } catch (e) {
     return false;
